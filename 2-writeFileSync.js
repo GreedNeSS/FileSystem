3921b3ac9403328f767800cee80688ff4a88d0ae
GreeDNeSS <ruslan199126@gmail.com>
2021-10-09 21:28:03 +0300

    'rm'

--- 2-writeFileSync.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-
-const data = fs.readFileSync('1-readFileSync.js', 'utf8');
-const lines = data.split('\r\n').filter(line => !!line);
-console.log({ data, lines });
-fs.writeFileSync('1-readFileSync.txt', lines.join('\n'));
